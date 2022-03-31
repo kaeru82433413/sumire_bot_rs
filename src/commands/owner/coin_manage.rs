d5fca2c9accd5742cd54563d45cbeff78af8f71d
kaeru82433413 <56305929+kaeru82433413@users.noreply.github.com>
2022-03-31 21:53:59 +0900

    [add] cnm setコマンドを追加

--- src/commands/owner/coin_manage.rs
+++ src/commands/owner/coin_manage.rs
@@ -13,7 +13,7 @@ use crate::utils::*;
 
 #[command]
 #[aliases(cn_manage, coinm, cnm)]
-#[sub_commands(add)]
+#[sub_commands(add, set)]
 async fn coin_manage(ctx: &Context, msg: &Message) -> CommandResult {
     msg.channel_id.say(&ctx, "正しいサブコマンドが指定されませんでした").await?;
     Ok(())
@@ -35,6 +35,29 @@ async fn add(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 
     msg.reply(ctx, format!("所持コインに{}を加算しました。\n{}", increase,
         strings::PointTransition::new(&target).increase(increase).after(target_data.point)
+        )).await?;
+
+    Ok(())
+}
+
+
+#[command]
+async fn set(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let mut args = ArgsWrapper(args);
+    let target = args.string()?;
+    let target = Member::convert(ctx, Some(crate::consts::SUMIRE_GUILD), None, &target).await?;
+    let value: i32 = args.parse()??;
+
+    let conn = get_connection(ctx).await;
+    let mut target_data = database::get_member_data(&conn, target.user.id.0 as i64)?;
+    let point_trans = strings::PointTransition::new(&target).before(target_data.point);
+
+    target_data.point = value;
+    diesel::update(&target_data).set(&target_data).execute(&conn)?;
+
+
+    msg.reply(ctx, format!("所持コインを{}にしました。\n{}", value,
+        point_trans.after(target_data.point)
         )).await?;
 
     Ok(())
