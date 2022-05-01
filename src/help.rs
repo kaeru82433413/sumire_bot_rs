a11d8583a677b8c04ee3e080d888f0746eeb8174
kaeru82433413 <56305929+kaeru82433413@users.noreply.github.com>
2022-05-01 15:57:10 +0900

    [fix] 無効なmod文を削除

--- src/help.rs
+++ src/help.rs
@@ -3,7 +3,6 @@ use serenity::prelude::TypeMapKey;
 mod command_list;
 pub use command_list::{CommandList, CommandSearchResult};
 pub mod embed;
-pub mod suggest;
 
 pub struct CommandListKey;
 impl TypeMapKey for CommandListKey {
